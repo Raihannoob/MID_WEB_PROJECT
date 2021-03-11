7bdf39e5b584c9d0bc22c93de57d1ef19b2fa5b1
Raihannoob <you@example.com>
2021-03-11 22:59:14 +0600

    logo home reg forgot

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PUBLIC HOME</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src='logo.jpg' alt="home" height='100px'/>
+            </td> 
+            <td align='right'>
+                    <nav>
+                        <a href='home.php'>Home</a> |
+                        <a href='login.php'>Log In</a> |
+                        <a href='registration.php'>Registration</a>
+                    </nav>
+            </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            <h1>
+                Welcome to Osude Ar Dokan DOt Com
+            </h1>
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+                Copyright © 2017
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+if (isset($_SESSION['flag'])) {
+    header('location: dashboard.php');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Login</title>
+</head>
+
+<body>
+    <fieldset>
+        <table width='100%'>
+            <tr>
+                <td>
+                    <img src='logo.jpg' height='100px' />
+                </td>
+                <td align='right'>
+                    <nav>
+                        <a href='home.php'>Home</a> |
+                        <a href='login.php'>Log In</a> |
+                        <a href='registration.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <legend>
+            LOGIN
+        </legend>
+        <form action='checklogin.php' method='POST'>
+                <table>
+                    <tr>
+                        <td>
+                            User Name:
+                        </td>
+                        <td>
+                            <input type="text" name='logusername'>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Password:
+                        </td>
+                        <td>
+                            <input type="password" name='logpassword'>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <hr>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type='checkbox'>Remember Me
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type='submit' value='Submit'>
+                            <a href='forgetpassword.php'>Forgot Password?</a>
+                        </td>
+                    </tr>
+                </table>
+        </form>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+                Copyright © 2021
+            </label>
+        </center>
+    </fieldset>
+</body>
+
+</html>
